8d0dee9356ea473b3b4516befbb7afcb38274f5d
KarenCampo777 <moviedo@wacoservices.com>
2021-07-22 18:12:21 -0500

    the lockboxes atre unlocked

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Lockboxes
+"""
+
+def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be unlocked
+    key_open --> stores the keys to unlock boxed
+    key --> interger that will iterate 
+    first_key --> the key that opens other boxes
+    """
+    key_open = [0]
+    for key in key_open: 
+        for first_key in boxes[key]:
+            if first_key < len(boxes) and not key_open:
+                first_key.append(key_open)
+
+        if boxes is None or len(boxes) == 0:
+            return False
+        else:
+            return True
+
+
