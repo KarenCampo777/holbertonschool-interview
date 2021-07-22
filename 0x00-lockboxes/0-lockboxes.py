9903f3fc2ea706afe468133e79da2ebe309d3179
KarenCampo777 <moviedo@wacoservices.com>
2021-07-22 18:35:05 -0500

    passed autopep

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -3,15 +3,16 @@
 Lockboxes
 """
 
+
 def canUnlockAll(boxes):
     """
     Determines if all boxes can be unlocked
     key_open --> stores the keys to unlock boxed
-    key --> interger that will iterate 
+    key --> interger that will iterate
     first_key --> the key that opens other boxes
     """
     key_open = [0]
-    for key in key_open: 
+    for key in key_open:
         for first_key in boxes[key]:
             if first_key < len(boxes) and not key_open:
                 first_key.append(key_open)
@@ -20,5 +21,3 @@ def canUnlockAll(boxes):
             return False
         else:
             return True
-
-
