39b77ec73eb34c3629a03e01376454dcc9cd96e5
KarenCampo777 <moviedo@wacoservices.com>
2021-07-22 21:51:42 -0500

    problem solved

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -10,14 +10,15 @@ def canUnlockAll(boxes):
     key_open --> stores the keys to unlock boxed
     key --> interger that will iterate
     first_key --> the key that opens other boxes
+    key_open == 0 and
     """
     key_open = [0]
     for key in key_open:
         for first_key in boxes[key]:
-            if first_key < len(boxes) and not key_open:
-                first_key.append(key_open)
+            if first_key not in key_open and key < len(boxes):
+                key_open.append(first_key)
 
-        if boxes is None or len(boxes) == 0:
-            return False
-        else:
-            return True
+    if len(key_open) != len(boxes):
+        return False
+    else:
+        return True
