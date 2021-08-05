998dd66a216c9964403c0820cb6698358d3831dc
KarenCampo777 <moviedo@wacoservices.com>
2021-08-05 18:11:52 -0500

    fixed identation and trailing whites spaces with betty

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -5,21 +5,18 @@
  * @value: The value to put in the new node or NULL on failure
  * return: pointer to the new node or NULL
  */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) 
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-        binary_tree_t *newNode;
-        /* Declaration of the  Variable that points to the structure */
-    
-        newNode = malloc(sizeof(binary_tree_t));
-        /*allocates the requested memory and returns a pointer to it.*/
-        if (newNode == NULL)
-            return (NULL);
-            /*validation of NULL*/
-
-        newNode->left = newNode->right = NULL;
-        newNode->n = value;
-        newNode->parent = parent;
-        /*node addition*/
-
-        return (newNode);
+	binary_tree_t *newNode;
+	/* Declaration of the  Variable that points to the structure */
+	newNode = malloc(sizeof(binary_tree_t));
+	/*allocates the requested memory and returns a pointer to it.*/
+	if (newNode == NULL)
+		return (NULL);
+	/*validation of NULL*/
+	newNode->left = newNode->right = NULL;
+	newNode->n = value;
+	newNode->parent = parent;
+	/*node addition*/
+	return (newNode);
 }
