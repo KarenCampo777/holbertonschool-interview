0cc09fb37c8cc8bc4e03341c59f736bf18182390
KarenCampo777 <moviedo@wacoservices.com>
2021-08-27 21:06:10 -0500

    corrections, corrections

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -2,16 +2,16 @@
 #include <stddef.h>
 
 /**
- * is_palindrome - Checks if a singly linked list is a palindrome.
+ * is_palindrome - Checks linked list is a palindrome.
  * @head: pointer
- * return: 0 if it is not a palindrome, 1 if it is a palindrome
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
  */
 
 int is_palindrome(listint_t **head)
 {
 	int longi = sizeof(head);
 	int init = 0;
-	int ending = longi -1;
+	int ending = longi - 1;
 
 while (head[init] == head[ending])
 {
