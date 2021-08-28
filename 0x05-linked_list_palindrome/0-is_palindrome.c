cb28fb6bb4b4126613a9a9194d182b489c545f76
KarenCampo777 <moviedo@wacoservices.com>
2021-08-27 19:53:18 -0500

    palindrome project

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stddef.h>
+
+/**
+ * is_palindrome - Checks if a singly linked list is a palindrome.
+ * @head: pointer
+ * return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+
+int is_palindrome(listint_t **head)
+{
+	int longi = sizeof(head);
+	int init = 0;
+	int ending = longi - 1;
+
+	if (longi <= 1)
+		return (1);
+
+while (head[init] == head[ending])
+{
+	if (init >= ending)
+		return (1);
+	init++;
+	ending--;
+}
+return (0);
+}
