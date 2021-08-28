fbabd853c9ed042a5cd08d591179b499d9697d45
KarenCampo777 <moviedo@wacoservices.com>
2021-08-27 21:03:20 -0500

    a correction

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -11,12 +11,15 @@ int is_palindrome(listint_t **head)
 {
 	int longi = sizeof(head);
 	int init = 0;
-	int ending = longi;
+	int ending = longi -1;
 
 while (head[init] == head[ending])
 {
 	if (longi == 1)
 		return (1);
+	else
+		return (0);
+
 	init++;
 	ending--;
 }
