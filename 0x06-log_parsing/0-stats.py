b31f96e4dfe9f2b7ffa2f0e8e2579a34e718689b
KarenCampo777 <moviedo@wacoservices.com>
2021-09-01 12:18:36 -0500

    finding the logic

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -3,10 +3,11 @@
 
 if __name__ == "__main__":
     from sys import stdin
+    
     random_codes = {200:0, 301:0, 400:0, 401:0, 403:0, 404:0, 405:0, 500:0}
     codes_size =0
-    def file_path(random_codes, code_size):
-        print("File size: {}".format(code_size))
+    def file_path(random_codes, codes_size):
+        print("File size: {}".format(codes_size))
     
     counter = 1
     for counter in random_codes:
@@ -16,9 +17,9 @@ if __name__ == "__main__":
     
     try:
         for line in stdin:
-            random_codes += 1
-            res = line.split()
-            c += int(res[-1])
+            codes_size += 1
+            res = line.split(" ")
+            codes_size += int(res[-1])
         if res[-2] in random_codes.keys():
             random_codes[res[-2]] += 1
     except Exception:
@@ -28,3 +29,4 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         file_path(counter, random_codes)
         raise
+    file_path()
