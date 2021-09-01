ec51f031a3ab727bab7200bad396d2e48e2e9547
KarenCampo777 <moviedo@wacoservices.com>
2021-09-01 18:25:17 -0500

    getting there

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,32 +1,34 @@
 #!/usr/bin/python3
 """python"""
-
+from sys import stdin
 if __name__ == "__main__":
-    from sys import stdin
+
     
-    random_codes = {200:0, 301:0, 400:0, 401:0, 403:0, 404:0, 405:0, 500:0}
-    codes_size =0
-    def file_path(random_codes, codes_size):
+    random_codes = {'200':0, '301':0, '400':0, '401':0, '403':0, '404':0, '405':0, '500':0}
+    codes_size = 0, counter = 1
+    
+    def file_path():
         print("File size: {}".format(codes_size))
-    
-    counter = 1
-    for counter in random_codes:
-        if counter in random_codes == 0:
-            continue
-        print("{}: {}".format(counter, random_codes))
-    
+
+    for key, value in sorted(codes_size.items()):
+        if value > 0:
+            print("{}: {}".format(key, value))
+
     try:
         for line in stdin:
-            codes_size += 1
-            res = line.split(" ")
-            codes_size += int(res[-1])
-        if res[-2] in random_codes.keys():
-            random_codes[res[-2]] += 1
-    except Exception:
-        pass
-        if not line % 10:
-                 file_path(counter, random_codes)
-    except KeyboardInterrupt:
-        file_path(counter, random_codes)
-        raise
-    file_path()
+            split_line = line.strip().split()
+            if len(split_line) < 7:
+                continue
+        codes_size += int(split_line[-1])
+        code_stats = split_line[-2]
+        if code_stats in code_stats:
+            code_stats[code_stats] += 1
+        if counter == 10:
+            file_path(code_stats, codes_size)
+            counter = 1
+        else:
+            counter += 1
+    file_path(code_stats, codes_size)
+except KeyboardInterrupt:
+    file_path(code_stats, codes_size)
+    raise
