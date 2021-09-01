f48fd87eecf7bfcdeb6b137562bf06b9f1f030ba
KarenCampo777 <moviedo@wacoservices.com>
2021-09-01 18:38:22 -0500

    done

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,34 +1,33 @@
 #!/usr/bin/python3
-"""python"""
+"""Script that reads stdin"""
 from sys import stdin
-if __name__ == "__main__":
 
-    
-    random_codes = {'200':0, '301':0, '400':0, '401':0, '403':0, '404':0, '405':0, '500':0}
-    codes_size = 0, counter = 1
-    
-    def file_path():
-        print("File size: {}".format(codes_size))
 
-    for key, value in sorted(codes_size.items()):
+def file_metrics(status_codes, file_size):
+    print("File size: {}".format(file_size))
+    for key, value in sorted(status_codes.items()):
         if value > 0:
-            print("{}: {}".format(key, value))
+            print("{}: {}".format(k, v))
 
-    try:
-        for line in stdin:
-            split_line = line.strip().split()
-            if len(split_line) < 7:
-                continue
+
+random_codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,'404': 0,'405': 0, '500': 0}
+counter = 1
+codes_size = 0
+try:
+    for line in stdin:
+        split_line = line.strip().split()
+        if len(split_line) < 7:
+            continue
         codes_size += int(split_line[-1])
         code_stats = split_line[-2]
-        if code_stats in code_stats:
-            code_stats[code_stats] += 1
+        if code_stats in random_codes:
+            random_codes[code_stats] += 1
         if counter == 10:
-            file_path(code_stats, codes_size)
+            file_metrics(random_codes, codes_size)
             counter = 1
         else:
             counter += 1
-    file_path(code_stats, codes_size)
+    file_metrics(random_codes, codes_size)
 except KeyboardInterrupt:
-    file_path(code_stats, codes_size)
+    file_metrics(random_codes, codes_size)
     raise
