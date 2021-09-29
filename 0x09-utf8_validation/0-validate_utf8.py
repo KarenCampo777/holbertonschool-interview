840919f2a1385c23e97df2bc18404858c7e87e39
KarenCampo777 <moviedo@wacoservices.com>
2021-09-28 23:52:28 -0500

    corrections

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
+"""validation of UTF8"""
+
+
 def validUTF8(data):
-    counter = 0
+    count = 0
     for x in data:
         mask = 0b10000000
-        if not counter:
+        if not count:
             while (mask & x):
-                counter += 1
+                count += 1
                 mask >>= 1
-            if counter > 4:
+            if count > 4:
                 return False
-            if counter:
-                counter -= 1
-                if counter == 0:
+            if count:
+                count -= 1
+                if count == 0:
                     return False
-        elif counter > 0:
+        elif count > 0:
             if x >> 6 != 2:
                 return False
-            counter -= 1
-    return not counter
+            count -= 1
+    return not count
+    
